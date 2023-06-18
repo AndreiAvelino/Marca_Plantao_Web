5a7c9b84be8febc90c24a12b76b00efc36fe4497
AndreiAvelino <58883452+AndreiAvelino@users.noreply.github.com>
2023-06-17 21:48:32 -0300

    trocando protocolo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "https://joaoremix3-001-site1.atempurl.com/"
+  api: "http://joaoremix3-001-site1.atempurl.com/"
   //api: "https://localhost:7046/"
 };
 
